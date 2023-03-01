fba723af5fe46107074ff42e27aa0811272ee849
teresarod11 <rodriguez49372@gmail.com>
2023-03-01 10:55:25 -0500

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,17 +14,61 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'text-editor.bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
+    plugins: [// Webpack plugin that generates our html file and injects our bundles. 
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Contact Cards'
+    }),
+   
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Contact Cards',
+      short_name: 'Contact',
+      description: 'Never forget your contacts!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader','css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
-  };
+  }
 };
