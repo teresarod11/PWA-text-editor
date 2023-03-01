288880d4681d20f71d96a82ae42ce84121dd3fed
teresarod11 <rodriguez49372@gmail.com>
2023-03-01 11:33:16 -0500

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '.bundle.js',
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [// Webpack plugin that generates our html file and injects our bundles. 
